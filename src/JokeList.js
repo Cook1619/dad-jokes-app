378d82cbba3d0702dfeb6e076da6ebe3792efac5
Cook1619 <cook6348@gmail.com>
2020-02-12 15:45:14 -0600

    Finished joke list styling

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,31 +1,40 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import './JokeList.css'
 
 class JokeList extends Component {
     static defaultProps = {
         numJokesToGet: 10
     }
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = { jokes: [] }
     }
     async componentDidMount() {
         //Load dad jokes
         let jokes = []
-        while (jokes.length < this.props.numJokesToGet){
+        while (jokes.length < this.props.numJokesToGet) {
             let res = await axios.get("https://icanhazdadjoke.com/",
-            {
-                headers: { Accept: 'application/json' }
-            });
+                {
+                    headers: { Accept: 'application/json' }
+                });
             jokes.push(res.data.joke)
-        }   
+        }
         this.setState({ jokes });
 
     }
     render() {
         return (
             <div className="JokeList">
-                <h1>Dad Jokes</h1>
+                <div className="JokeList-sidebar">
+                    <h1 className="JokeList-title">
+                        <span>Dad</span> Jokes
+                    </h1>
+                    <img src='https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg' />
+                    <button className='JokeList-getmore' onClick={this.handleClick}>
+                        Fetch Jokes
+          </button>
+                </div>
                 <div className="JokeList-jokes">
                     {this.state.jokes.map(jokes => (
                         <div>{jokes}</div>
